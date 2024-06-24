eb65d07a70402d3d9403394394b199aa9dc93f26
Aurélien Dufour <aureluendufour7@gmail.com>
2024-06-24 21:32:30 +0200

    initial commit

---
+++ src/main/java/com/camping_fisa/bouffonduroiapi/BouffonduroiApplication.java
@@ -0,0 +1,13 @@
+package com.camping_fisa.bouffonduroiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BouffonduroiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BouffonduroiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/camping_fisa/bouffonduroiapi/BouffonduroiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camping_fisa.bouffonduroiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BouffonduroiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
