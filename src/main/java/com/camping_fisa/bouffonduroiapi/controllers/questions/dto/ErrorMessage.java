5de8b68c94e85117adbb7693fd022ee33e788ad2
MathieuSymphorien <72963501+MathieuSymphorien@users.noreply.github.com>
2024-11-26 18:47:45 +0100

    BREAKING CHANGE: controllers / services / entities / db are deleted

--- src/main/java/com/camping_fisa/bouffonduroiapi/controllers/questions/dto/ErrorMessage.java
+++
@@ -1,8 +0,0 @@
-package com.camping_fisa.bouffonduroiapi.controllers.questions.dto;
-
-public class ErrorMessage {
-
-    public String message;
-
-    public ErrorMessage(String message) { this.message = message; }
-}
