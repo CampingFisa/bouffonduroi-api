5e19232ee448d6dbb7a59058f632105b6ba3631a
MathieuSymphorien <72963501+MathieuSymphorien@users.noreply.github.com>
2024-12-18 11:08:52 +0100

    fix: 💚delete unit test

--- src/test/java/com/camping_fisa/bouffonduroiapi/BouffonduroiApplicationTests.java
+++ src/test/java/com/camping_fisa/bouffonduroiapi/BouffonduroiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BouffonduroiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
